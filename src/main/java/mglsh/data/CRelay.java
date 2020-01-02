5ccc6bc032b339e1b050198842ab1246acf6d46f
Misha <mglsh@ukr.net>
2020-01-03 00:48:42 +0200

    Version 001

---
+++ src/main/java/mglsh/data/CRelay.java
@@ -0,0 +1,14 @@
+package mglsh.data;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class CRelay {
+    private String CRelayId;
+    private String CRelayName;
+    private String CRelayParameter;
+
+
+
+}
